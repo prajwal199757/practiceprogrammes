c94b7bc513b0c49f99de7080f2c2b937e201531f
prajwal <prajwalsn@maveric-systems.com>
2022-01-14 13:31:23 +0530

    programme1 updated with print statement

--- src/Arrays/Programme1.java
+++ src/Arrays/Programme1.java
@@ -8,6 +8,7 @@ public class Programme1 {
         String s1="i.like.this.program.very.much";
         StringBuilder s2=new StringBuilder();
         String[] s3 = (s1.split("\\."));
+        System.out.println("hello");
         for (int i=s3.length-1;i>=0 ;i--)
         {
             s2=s2.append(s3[i]);
